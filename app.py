2b38f4efe424adb4381a001910846ebe39d2d824
root <root@BOOK-N43SS40OCT.localdomain>
2022-06-15 14:11:28 +0900

    ch6.database

---
+++ app.py
@@ -0,0 +1,74 @@
+from flask import Flask, request, jsonify
+from sqlalchemy import create_engine
+from util import *
+from api import *
+
+def create_app(test_config = None):
+    app = Flask(__name__)
+    app.json_encoder = CustomJSONEncoder
+
+    if test_config is None:
+        app.config.from_pyfile("config.py")
+    else:
+        app.config.update(test_config)
+
+    database = create_engine(app.config['DB_URL'], encoding = 'utf-8', max_overflow = 0)
+    app.database = database
+
+    @app.route("/", methods=['GET'])
+    def index():
+        return "Hello Flask"
+
+
+    @app.route("/ping", methods=['GET'])
+    def ping():
+        return "pong"
+
+
+    @app.route("/sign-up", methods=['POST'])
+    def sign_up():
+        new_user     = request.json
+        new_user_id  = insert_user(new_user)
+        new_user = get_user(new_user_id)
+
+        return jsonify(new_user)
+
+
+    @app.route("/tweet", methods=['POST'])
+    def tweet():
+        user_tweet = request.json
+        tweet = user_tweet['tweet']
+
+        if len(tweet) > 300:
+            return "300자를 초과했습니다.", 400
+
+        insert_tweet(user_tweet)
+
+        return "", 200
+
+
+    @app.route("/follow", methods=['POST'])
+    def follow():
+        payload = request.json
+        insert_follow(payload)
+
+        return '', 200
+
+
+    @app.route("/unfollow", methods=['POST'])
+    def unfollow():
+        payload = request.json
+        insert_unfollow(payload)
+
+        return '', 200
+
+
+    @app.route("/timeline/<int:user_id>", methods=['GET'])
+    def timeline(user_id):
+        return jsonify({
+            'user_id' : user_id,
+            'timeline' : get_timeline(user_id)
+        })
+    
+
+    return app
---
+++ util.py
@@ -0,0 +1,11 @@
+from flask.json import JSONEncoder
+
+## Default JSON encoder는 set을 JSON으로 변환할 수 없다.
+## 그러므로 커스텀 엔코더를 작성해서 set을 list로 변환하여
+## JSON으로 변환 가능하게 해주어야 한다.
+class CustomJSONEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, set):
+            return list(obj)
+
+        return JSONEncoder.default(self, obj)
